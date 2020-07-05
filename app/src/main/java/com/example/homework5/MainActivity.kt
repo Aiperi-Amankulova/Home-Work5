2b9e04806da2fcebcc3157774715524bab3e5d98
Aiperi <aip.amankulova@gmail.com>
2020-07-05 23:25:36 +0600

    Adding toast

--- app/src/main/java/com/example/homework5/MainActivity.kt
+++ app/src/main/java/com/example/homework5/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.homework5
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,23 +23,35 @@ class MainActivity : AppCompatActivity() {
         val preference = getSharedPreferences("HomeWork", Context.MODE_PRIVATE)
 
         btnSignIn.setOnClickListener {
+
             val intent = Intent(applicationContext,  RegistrationActivity::class.java)
 
             startActivity(intent)
 
-        }
+
+            val textInSecondActivity  =  preference.getString("text","not saved")
+            loginINFirstActivity.setText(textInSecondActivity)
+
+            val textInSecondActivity2 =  preference.getString("text2","not saved")
+            passwordInFirstActivity.setText(textInSecondActivity2 )
+
+                finish()
+            }
+
         btnSignUp.setOnClickListener {
             val textToLogin = loginINFirstActivity.text.toString()
             val textToPassword = passwordInFirstActivity.text.toString()
             val text = "Data saved"
             val duration = Toast.LENGTH_SHORT
 
-            preference.edit().putString("login", textToLogin).apply()
+
+            preference.edit().putString("login",  textToLogin).apply()
             preference.edit().putString("password", textToPassword).apply()
 
             Toast.makeText(applicationContext, text, duration).show()
 
         }
+
 
         startActivity(intent)
         }
--- app/src/main/java/com/example/homework5/RegistrationActivity.kt
+++ app/src/main/java/com/example/homework5/RegistrationActivity.kt
@@ -1,11 +1,57 @@
 package com.example.homework5
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val loginInSecondActivity = findViewById<EditText>(R.id.loginInSecondActivity)
+        val passwordInSecondActivity = findViewById<EditText>(R.id.passwordInSecondActivity)
+        val btnUpdate = findViewById<Button>(R.id.btnUpdate)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        val prefInSecondActivity = getSharedPreferences("HomeWork5", Context.MODE_PRIVATE)
+        val preference = getSharedPreferences("HomeWork", Context.MODE_PRIVATE)
+
+        val textInFirstActivity  =  preference.getString("text","not saved")
+        loginInSecondActivity.setText(textInFirstActivity)
+
+        val textInFirstActivity2 =  preference.getString("text2","not saved")
+        passwordInSecondActivity.setText(textInFirstActivity2 )
+
+        btnUpdate.setOnClickListener {
+            val textToLogin = loginInSecondActivity.text.toString()
+            val textToPassword = passwordInSecondActivity.text.toString()
+
+            preference.edit().putString("login",  textToLogin).apply()
+            preference.edit().putString("password", textToPassword).apply()
+
+        }
+
+
+        btnLogout.setOnClickListener {
+            val textToLoginInSecondActivity = loginInSecondActivity.text.toString()
+            val textToPasswordInSecondActivity = passwordInSecondActivity.text.toString()
+
+            prefInSecondActivity.edit().putString("login", textToLoginInSecondActivity).apply()
+            prefInSecondActivity.edit().putString("password", textToPasswordInSecondActivity).apply()
+
+            val intent = Intent( this, MainActivity::class.java)
+            intent.putExtra("textToLoginInSecondActivity", textToLoginInSecondActivity)
+            intent.putExtra("textToPasswordInSecondActivity", textToPasswordInSecondActivity)
+
+
+            setResult(Activity.RESULT_OK, intent)
+            }
+        }
+
     }
-}
